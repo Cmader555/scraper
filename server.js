ce971bf0fba0a6166f11518aea418126158d739a
Christian Mader <Cmader555@users.noreply.github.com>
2019-06-09 11:30:31 -0700

    set up server

---
+++ server.js
@@ -0,0 +1,29 @@
+// Dependencies
+const express = require("express");
+const mongojs = require("mongojs");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const exphbs = require("express-handlebars");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
